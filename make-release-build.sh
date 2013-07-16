f12e6d344a688323dba86767757dbfd1f298c7b1
Hans-Christoph Steiner <hans@eds.org>
2013-07-15 23:15:24 -0400

    fix release script to work on the build machine

--- make-release-build.sh
+++ make-release-build.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
+set -e
+
 . ~/.android/bashrc
 
+projectroot=`pwd`
 projectname=`sed -n 's,.*name="app_name">\(.*\)<.*,\1,p' app/res/values/strings.xml`
 
-cd external/ActionBarSherlock
+cd $projectroot/external/ActionBarSherlock
 git reset --hard
 git clean -fdx
-cd external/F5Android
+cd $projectroot/external/F5Android
 git reset --hard
 git clean -fdx
-cd ../..
+cd $projectroot
 git reset --hard
 git clean -fdx
 git submodule update --init --recursive
 
-cp ~/.android/ant.properties .
-
+cp ~/.android/ant.properties $projectroot/app/
 ./setup-ant.sh
+
+cd $projectroot/app/
 ant release
 
-gpg --detach-sign bin/$projectname-release.apk
+apk=$projectroot/app/bin/$projectname-release.apk
+if [ -e $apk ]; then
+    gpg --detach-sign $apk
+else
+    echo $apk does not exist!
+fi
